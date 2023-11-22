32cfd8ac2f76efeb98f808d25d0f068391578d9c
basdevelop <hyperorchid@outlook.com>
2023-11-22 18:02:12 +0800

    use template to render html

--- assets/js/friends.js
+++ assets/js/friends.js
@@ -1,3 +1,32 @@
-function LoadCachedFriedList(){
-    return ["NJA1fmxxVFRY2XWvcPU41zfxMrjb2iXDzaRW4jSD1gVCFg","NJJ5ryLVoNG9Cm9yaPheMQH4tpUYoGyKYXGWNfFqLTFGLP"];
+function LoadCachedFriedList() {
+    return ["NJA1fmxxVFRY2XWvcPU41zfxMrjb2iXDzaRW4jSD1gVCFg", "NJJ5ryLVoNG9Cm9yaPheMQH4tpUYoGyKYXGWNfFqLTFGLP"];
+}
+
+class messageTipsItem {
+    constructor(address, avatarUrl, nickname, time, description) {
+        this.address = address;
+        this.avatarUrl = avatarUrl;
+        this.nickname = nickname;
+        this.time = time;
+        this.description = description;
+    }
+}
+
+function cacheLoadCachedMsgList() {
+    const result = [];
+    const item_1 = new messageTipsItem("NJA1fmxxVFRY2XWvcPU41zfxMrjb2iXDzaRW4jSD1gVCFg",
+        "/assets/logo.png", "日本聪",
+        new Date(), "文本消息");
+    const item_2 = new messageTipsItem("NJJ5ryLVoNG9Cm9yaPheMQH4tpUYoGyKYXGWNfFqLTFGLP",
+        "/assets/logo.png", "中本聪",
+        new Date(), "文本消息");
+    const item_3 = new messageTipsItem("NJA1fmxxVFRY2XWvcPU41zfxMrjb2iXDzaRW4jSD1gVCFg",
+        "/assets/logo.png", "V神",
+        new Date(), "文本消息");
+
+    result.push(item_1);
+    result.push(item_2);
+    result.push(item_3);
+
+    return result
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,84 +1,37 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 	"path/filepath"
 )
 
-var templates map[string]*template.Template
-
 var (
-	simpleRouterMap = map[string]*RouterDataApi{
-		"/":             &RouterDataApi{"assets/index.html", nil},
-		"/index":        {"assets/index.html", nil},
-		"/registration": {"assets/registration.html", nil},
-		"/main":         {"assets/main.html", nil},
-		"/showWallet":   {"assets/showWallet.html", nil},
-	}
+	staticFileDir = "assets"
 )
 
-type RouterData func() any
-
-type RouterDataApi struct {
-	Path     string
-	DataFunc RouterData
-}
-
-func init() {
-	// 初始化模板map
-	templates = make(map[string]*template.Template)
-
-	// 一次性解析所有匹配模式的HTML文件
-	templateFiles, err := filepath.Glob("assets/*.html")
-	if err != nil {
-		panic(err)
-	}
-
-	// 遍历HTML文件并解析模板
-	for _, file := range templateFiles {
-		templateName := filepath.Base(file)
-		templates[templateName] = template.Must(template.ParseFiles(file))
-	}
-}
-
-// renderTemplate 加载并渲染HTML模板
-func renderTemplate(w http.ResponseWriter, templateName string, data interface{}) {
-	// 获取模板
-	tmpl, ok := templates[templateName]
-	if !ok {
-		http.Error(w, "Template not found", http.StatusInternalServerError)
-		return
-	}
-
-	// 渲染HTML模板
-	err := tmpl.Execute(w, data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func simpleRouter(api *RouterDataApi) func(http.ResponseWriter, *http.Request) {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		// 获取模板的名称（不带路径和扩展名）
-		templateName := filepath.Base(api.Path)
-
-		// 渲染HTML模板
-		var data any
-		if api.DataFunc != nil {
-			data = api.DataFunc
-		}
-		renderTemplate(writer, templateName, data)
-	}
+var simpleRouterMap = map[string]string{
+	"/":             "index.html",
+	"/index":        "index.html",
+	"/registration": "registration.html",
+	"/main":         "main.html",
+	"/showWallet":   "showWallet.html",
 }
 
 func main() {
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
-	// 设置路由处理函数
-	for route, api := range simpleRouterMap {
-		http.HandleFunc(route, simpleRouter(api))
+	// 设置静态文件服务
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir(staticFileDir))))
 
+	// 设置路由处理函数
+	for route, fileName := range simpleRouterMap {
+		http.HandleFunc(route, simpleRouter(fileName))
 	}
+
 	// 启动Web服务器
 	http.ListenAndServe(":80", nil)
+}
+
+func simpleRouter(fileName string) func(http.ResponseWriter, *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		http.ServeFile(writer, request, filepath.Join(staticFileDir, fileName))
+	}
 }

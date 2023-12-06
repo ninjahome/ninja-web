cf1d0a7957e039e07c424ccd291a62e60670956d
basdevelop <hyperorchid@outlook.com>
2023-12-06 08:17:45 +0800

    refactor main go

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"os"
 	"path/filepath"
 	"strings"
 	"time"
@@ -23,31 +20,12 @@ var simpleRouterMap = map[string]string{
 	"/showWallet":   "showWallet.html",
 }
 
-type ApiEthBalanceData struct {
-	Address string `json:"address"`
-	Eth     int64  `json:"eth"`
-	Usdt    int64  `json:"usdt"`
-}
-
-func handleBalanceRequest(w http.ResponseWriter, r *http.Request) {
-	accountID := r.URL.Query().Get("accountID")
-
-	balance := &ApiEthBalanceData{
-		Address: accountID,
-		Eth:     0,
-		Usdt:    0,
-	}
-	bts, _ := json.Marshal(balance)
-	_, _ = w.Write(bts)
-}
-
 func main() {
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.HandlerFunc(assetsRouter)))
-
+	//http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir(staticFileDir))))
 	for route, fileName := range simpleRouterMap {
 		http.HandleFunc(route, simpleRouter(fileName))
 	}
-	http.HandleFunc("/balance", handleBalanceRequest)
 	panic(http.ListenAndServe(":80", nil))
 }
 
@@ -57,42 +35,7 @@ func simpleRouter(fileName string) func(http.ResponseWriter, *http.Request) {
 		writer.Header().Set("Access-Control-Allow-Origin", "*")
 		writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-		// 获取文件信息
-		filePath := filepath.Join(staticFileDir, fileName)
-		file, err := http.Dir(staticFileDir).Open(fileName)
-		if err != nil {
-			http.Error(writer, "File not found", http.StatusNotFound)
-			return
-		}
-		defer file.Close()
-
-		// 获取文件修改时间
-		fileInfo, err := file.Stat()
-		if err != nil {
-			http.Error(writer, "Unable to get file information", http.StatusInternalServerError)
-			return
-		}
-		modTime := fileInfo.ModTime()
-
-		// 根据控制变量 shouldReload 设置 Cache-Control 头
-		if shouldReload {
-			writer.Header().Set("Cache-Control", "max-age=0, no-store, must-revalidate")
-		} else {
-			writer.Header().Set("Cache-Control", "public, max-age=1036000") // 缓存10天
-		}
-
-		// 设置 Last-Modified 头
-		writer.Header().Set("Last-Modified", modTime.UTC().Format(http.TimeFormat))
-
-		// 检查是否需要重新加载
-		if t, err := time.Parse(http.TimeFormat, request.Header.Get("If-Modified-Since")); err == nil && modTime.Before(t.Add(1*time.Second)) {
-			writer.WriteHeader(http.StatusNotModified)
-			return
-		}
-
-		// 服务文件
-		http.ServeFile(writer, request, filePath)
+		serveStaticFile(writer, request, fileName)
 	}
 }
 
@@ -102,37 +45,32 @@ func assetsRouter(writer http.ResponseWriter, request *http.Request) {
 	if strings.HasSuffix(realUrlPath, ".map") {
 		realUrlPath = strings.TrimSuffix(realUrlPath, ".map")
 	}
-	// 打开文件
-	filePath := filepath.Join(staticFileDir, realUrlPath)
 
-	// 打开文件
-	file, err := http.Dir(staticFileDir).Open(realUrlPath)
+	serveStaticFile(writer, request, realUrlPath)
+}
+
+func serveStaticFile(writer http.ResponseWriter, request *http.Request, fileName string) {
+	// 获取文件信息
+	filePath := filepath.Join(staticFileDir, fileName)
+	file, err := http.Dir(staticFileDir).Open(fileName)
 	if err != nil {
-		if os.IsNotExist(err) {
-			// 文件不存在的情况
-			fmt.Printf("File not found: %s\n", realUrlPath)
-		} else {
-			// 其他错误
-			fmt.Printf("Error opening file: %s\n", err)
-		}
 		http.Error(writer, "File not found", http.StatusNotFound)
 		return
 	}
 	defer file.Close()
 
-	// 获取文件信息
+	// 获取文件修改时间
 	fileInfo, err := file.Stat()
 	if err != nil {
 		http.Error(writer, "Unable to get file information", http.StatusInternalServerError)
 		return
 	}
 	modTime := fileInfo.ModTime()
-
-	// 设置 Cache-Control 头
+	// 根据控制变量 shouldReload 设置 Cache-Control 头
 	if shouldReload {
 		writer.Header().Set("Cache-Control", "max-age=0, no-store, must-revalidate")
 	} else {
-		writer.Header().Set("Cache-Control", "public, max-age=1036000") //缓存10天
+		writer.Header().Set("Cache-Control", "public, max-age=1036000") // 缓存10天
 	}
 
 	// 设置 Last-Modified 头
